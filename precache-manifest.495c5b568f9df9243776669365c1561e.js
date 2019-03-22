20d4f3534a6b607e733601c4a1737381808fffbb
Leandro Haruki <leandroharuki@gmail.com>
2019-03-22 07:49:36 -0300

    Updates

---
+++ precache-manifest.495c5b568f9df9243776669365c1561e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0d3e1fd067066bf464fdc65b2d2caae",
+    "url": "/Noob/static/media/le-potato.f0d3e1fd.jpg"
+  },
+  {
+    "revision": "9c2718e5ef38410f4271",
+    "url": "/Noob/static/js/runtime~main.b342dd6a.js"
+  },
+  {
+    "revision": "b5e7344d9d0021abe939",
+    "url": "/Noob/static/js/main.5ec10de8.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/Noob/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "b5e7344d9d0021abe939",
+    "url": "/Noob/static/css/main.1f88e509.chunk.css"
+  },
+  {
+    "revision": "1b0d0522cda042308d51b9fa681b7f37",
+    "url": "/Noob/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Noob/precache-manifest.9eccaf84f00e2873ffdc63c95c7a954a.js"
+  "/Noob/precache-manifest.495c5b568f9df9243776669365c1561e.js"
 );
 
 workbox.clientsClaim();
