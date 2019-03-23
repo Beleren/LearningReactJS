4c9de51ec6b01e9004dc229153156cfc14ee8220
Leandro Haruki <leandroharuki@gmail.com>
2019-03-23 18:37:00 -0300

    Changing browser router to hash router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import React, { Component, Suspense, lazy } from "react";
 import Home from "./Home";
 
